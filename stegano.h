0173ef26def875593604c762eb806cd03120a744
Grégoire RIVIERE <gregoirer42@gmail.com>
2018-03-21 22:19:59 +0100

    Revert "Revert "[BUG] Correction ecriture bit""
    This reverts commit c67efcaee22641e80df8717382d88645492d24e0.

---
+++ stegano.h
@@ -0,0 +1,14 @@
+#ifndef STENO_GUARD
+#define STENO_GUARD
+
+#define TAILLE_ENTETE   54
+
+    /* Fonction principale d'ecriture de message */
+    void run_hide_mess(char* message, FILE* IMG_SRC, FILE* IMG_DEST);
+    /* Fonction d'ecriture pour un bit du message */
+    void hiding(unsigned char byte_message, int byte_number, unsigned char byte_img, unsigned char* byte_dest);
+    /* Ecriture des entetes */
+    void ecriture_header(FILE* IMG_SRC, FILE* IMG_DEST);
+    /* Ecriture de la fin de l'image source */
+    void ecrire_reste(FILE* IMG_SRC, FILE* IMG_DEST);
+#endif
